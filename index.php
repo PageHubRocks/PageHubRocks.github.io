bd05783aaa5d45d98ca6c6b526e5d6ff8a664fce
Fredric Castelius <fredric@castelius.se>
2014-10-14 19:37:17 +0200

    MailChimp signup added

--- index.php
+++ index.php
@@ -4,7 +4,22 @@
 	    <div>
 	        <h1 id="title">It's time for some change.</h1>
 	        <h2 id="sub-title">Next gen web making in progess.</h2>
-	    </div>
+			<div id="mc_embed_signup">
+				<form action="//rocks.us9.list-manage.com/subscribe/post?u=864d6e8508b833c79a1ea1303&amp;id=0aa783b25a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				    <div id="mc_embed_signup_scroll">
+						<div class="mc-field-group">
+							<input type="email" value="" placeholder="Your e-mail address here" name="EMAIL" class="required email" id="mce-EMAIL">
+						</div>
+						<div id="mce-responses" class="clear">
+							<div class="response" id="mce-error-response" style="display:none"></div>
+							<div class="response" id="mce-success-response" style="display:none"></div>
+						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					    <div style="position: absolute; left: -5000px;"><input type="text" name="b_864d6e8508b833c79a1ea1303_0aa783b25a" tabindex="-1" value=""></div>
+					    <div class="clear"><input type="submit" value="Get notified" name="subscribe" id="mc-embedded-subscribe" class="button"><span>»</span></div>
+				    </div>
+				</form>
+			</div>
+	    </div>		
 	</section>
 
 	<?php include 'footer.php';?>
